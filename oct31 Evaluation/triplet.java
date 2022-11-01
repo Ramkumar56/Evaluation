3d5cce542d265688b8539f1c581462def4b2f0c2
Ramkumar Dhamodharan <64086675+Ramkumar56@users.noreply.github.com>
2022-11-01 14:46:18 +0530

    Update triplet.java

--- oct31 Evaluation/triplet.java
+++ oct31 Evaluation/triplet.java
@@ -2,25 +2,23 @@ import java.util.*;
 
 public class triplet {
     public static void main(String[] args) {
-     int[] arr={-1,-1,-1,2,-1};
+     int[] arr={-1,-1,-1,2,0,-1,1};
         Set<List<Integer>>set=new HashSet<>();
         List<Integer>a=new ArrayList<>();
         Arrays.sort(arr);
-     int i=0;
-         int j=i+1;
-            int k=j+1;
-            while (i<arr.length && j< arr.length && k< arr.length){
-                 if (arr[i] + arr[j] + arr[k] == 0) {
-                     a.add(arr[i]);
-                     a.add(arr[j]);
-                     a.add(arr[k]);
-                     set.add(a);
-                 }
-                 a=new ArrayList<>();
-                 i++;
-                 j++;
-                 k++;
-     }
+           for(int i=0;i< arr.length;i++) {
+               for (int j = i + 1; j < arr.length; j++) {
+                   for (int k =j+1; k < arr.length; k++) {
+                       if (arr[i] + arr[j] + arr[k] == 0) {
+                           a.add(arr[i]);
+                           a.add(arr[j]);
+                           a.add(arr[k]);
+                           set.add(a);
+                       }
+                       a = new ArrayList<>();
+                   }
+               }
+           }
         System.out.println(set);
     }
 }
